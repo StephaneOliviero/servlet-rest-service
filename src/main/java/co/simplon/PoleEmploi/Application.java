d016e5b002fe1e37fce6983106bea3c5c271fec7
Stephane <Stephane@example.com>
2017-10-09 13:58:21 +0200

    Etapes 1,2,3,5

--- src/main/java/co/simplon/PoleEmploi/Application.java
+++ src/main/java/co/simplon/PoleEmploi/Application.java
@@ -3,26 +3,9 @@ package co.simplon.PoleEmploi;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-
 public class Application {
 
 	public static final EntityManagerFactory EMF = getEntityManagerFactory();
-
-	public static void main(String args[]) throws Exception {
-
-		Server server = new Server(9093);
-
-		WebAppContext webAppContext = new WebAppContext();
-		webAppContext.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-		webAppContext.setResourceBase("src/main/webapp/");
-		webAppContext.setContextPath("/servlet-rest-service");
-		server.setHandler(webAppContext);
-			
-		server.start();
-		server.join();
-	}
 
 	private static EntityManagerFactory getEntityManagerFactory() {
 		return Persistence.createEntityManagerFactory("BasePatrimoineLocale");
